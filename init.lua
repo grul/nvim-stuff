425af71d14005ac5e14ef0482f72ccddabdbf6a3
grul <git@localhost>
2026-02-08 11:22:11 +0100

    add the files

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- Clone lazy nvim if it does not exist already
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+		-- { "plugins.rustyrust" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{
+			"ellisonleao/gruvbox.nvim",
+			lazy = false, -- make sure we load this during startup if it is your main colorscheme
+			priority = 1000, -- make sure to load this before all the other start plugins
+			config = function()
+				vim.cmd([[colorscheme gruvbox]])
+			end,
+		},
+	},
+})
