6c0c7de8f0fc47a53710a134c9ad3730ad5592a1
grul <git@localhost>
2026-05-04 14:14:47 +0200

    add a diff viewer

--- init.lua
+++ init.lua
@@ -273,6 +273,42 @@ require("lazy").setup({
         end,
     },
 
+    -- Git signs in the gutter + hunk actions
+    {
+        "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gs = require("gitsigns")
+                    local function map(mode, lhs, rhs, desc)
+                        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+                    end
+
+                    map("n", "<leader>gl", function() gs.nav_hunk("next") end, "Next hunk")
+                    map("n", "<leader>gh", function() gs.nav_hunk("prev") end, "Previous hunk")
+                    map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+                    map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
+                    map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
+                    map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame line")
+                    map("n", "<leader>gB", gs.toggle_current_line_blame, "Toggle inline blame")
+                end,
+            })
+        end,
+    },
+
+    -- Full-file / project-wide diff view
+    {
+        "sindrets/diffview.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory", "DiffviewToggleFiles" },
+        keys = {
+            { "<leader>gd", "<cmd>DiffviewOpen<cr>",          desc = "Open diff view" },
+            { "<leader>gD", "<cmd>DiffviewClose<cr>",         desc = "Close diff view" },
+            { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (current file)" },
+        },
+    },
+
     -- TROUBLE
     {
         "folke/trouble.nvim",
